ab5a351eea94e0c4b17983f4ad10b32d44adccb4
Maksim <lyzlov023@gmail.com>
2023-12-06 21:06:01 +1000

    added error handler in settings

--- cloude-app/settings.py
+++ cloude-app/settings.py
@@ -20,14 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+try:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", '').split(" ")
 
-print(ALLOWED_HOSTS)
 # Application definition
 LOGIN_REDIRECT_URL = '/storage/'
 
